9aa590bf512adfc719d282f011cb00b11d8a2c69
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-06-17 12:29:33 +0900

    a

--- vaelstm_before_optuna.py
+++ vaelstm_before_optuna.py
@@ -290,12 +290,12 @@ def train(epoch):
         tmp = []
 
         
-        for j in range(2):
+        for j in range(3):
             tmp.append(torch.from_numpy(data[j]).to(device))
 
 
         optimizer.zero_grad()
-        recon_batch, mu, logvar = model(tmp[0], tmp[1], data[2])
+        recon_batch, mu, logvar = model(tmp[0], tmp[1], tmp[2])
         loss = loss_function(recon_batch, tmp[1], mu, logvar)
         loss.backward()
         train_loss += loss.item()
@@ -323,11 +323,11 @@ def test(epoch):
             tmp = []
 
      
-            for j in range(2):
+            for j in range(3):
                 tmp.append(torch.tensor(data[j]).to(device))
             
 
-            recon_batch, mu, logvar = model(tmp[0], tmp[1], data[2])
+            recon_batch, mu, logvar = model(tmp[0], tmp[1], tmp[2])
             test_loss += loss_function(recon_batch, tmp[1], mu, logvar).item()
             f0_loss += calc_lf0_rmse(recon_batch.cpu().numpy().reshape(-1, 199), tmp[1].cpu().numpy().reshape(-1, 199), lf0_start_idx, vuv_start_idx)
 
