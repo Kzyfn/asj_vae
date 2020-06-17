9cc319bc776880fa83a2fcdfcf1eb4fe2d77f6f0
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-06-17 15:15:41 +0900

    整理

--- vqvaelstm_sum.py
+++ vqvaelstm_sum.py
@@ -199,7 +199,7 @@ import pandas as pd
 
 mora_index_lists = sorted(glob(join('data/basic5000/mora_index', "squeezed_*.csv")))
 #mora_index_lists = mora_index_lists[:len(mora_index_lists)-5] # last 5 is real testset
-mora_index_lists_for_model = [np.array(pd.read_csv(path)).reshape(-1) for path in mora_index_lists]
+mora_index_lists_for_model = [np.loadtxt(path).reshape(-1) for path in mora_index_lists]
 
 train_mora_index_lists = []
 test_mora_index_lists = []
