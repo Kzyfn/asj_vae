0a0616f06a8656f283f099e3f2b8c7c31cda66f6
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-06-18 17:38:12 +0900

    a

--- util.py
+++ util.py
@@ -158,7 +158,7 @@ def create_loader(test=False):
         else:
             train_mora_index_lists.append(mora_i)
 
-    print(X["acoustic"])
+    print(X["acoustic"]["train"])
     X_acoustic_train = [
         minmax(x, X_min["train"], X_max["train"], feature_range=(0.01, 0.99))
         for x in X["acoustic"]["train"]
