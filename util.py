5a3aa25912a89385f78cad5ebc79f681f1c7ec8c
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-06-19 18:42:51 +0900

    loss修正

--- util.py
+++ util.py
@@ -97,6 +97,7 @@ def test(epoch, model, test_loader, loss_function):
     test_loss /= len(test_loader)
     f0_loss /= len(test_loader)
     f0_loss = f0_loss * 1200 / np.log(2)
+    print("f0_loss")
     print(f0_loss)
     print("====> Test set loss: {:.4f}".format(test_loss))
 
