1338cab9f9cba3eab6b66422945fbcde603a3689
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-06-19 10:47:21 +0900

    mora_i_back

--- models.py
+++ models.py
@@ -74,7 +74,10 @@ class VAE(nn.Module):
         x = F.relu(x)
 
         out, hc = self.lstm1(x.view(x.size()[0], 1, -1))
-        out = out[mora_index]
+        out_forward = out[:, :, :400][mora_index]
+        mora_index_for_back = np.concatenate([[0], mora_index[:-1] + 1])
+        out_back = out[:, :, 400:][mora_index_for_back]
+        out = torch.cat([out_forward, out_back], dim=2)
 
         h1 = F.relu(out)
 
@@ -173,7 +176,10 @@ class VQVAE(nn.Module):
         x = self.fc11(x)
         x = F.relu(x)
         out, hc = self.lstm1(x.view(x.size()[0], 1, -1))
-        out = out[mora_index]
+        out_forward = out[:, :, :400][mora_index]
+        mora_index_for_back = np.concatenate([[0], mora_index[:-1] + 1])
+        out_back = out[:, :, 400:][mora_index_for_back]
+        out = torch.cat([out_forward, out_back], dim=2)
 
         h1 = F.relu(out)
 
