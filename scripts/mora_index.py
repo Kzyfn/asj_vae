87c971b75b5f033b040dd4c1b9f2b650804c4d43
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-06-23 13:27:05 +0900

    mora_index

--- scripts/mora_index.py
+++ scripts/mora_index.py
@@ -1,3 +1,11 @@
+import sys
+
+# sys.path.remove('/usr/local/lib/python3.7/site-packages')
+sys.path.append("/usr/local/.pyenv/versions/3.6.0/lib/python3.6/site-packages")
+sys.path.append(
+    "/Users/kazuya_yufune/.pyenv/versions/3.6.0/lib/python3.6/site-packages"
+)
+
 import numpy as np
 from nnmnkwii.io import hts
 from os.path import join
@@ -5,29 +13,49 @@ from glob import glob
 from tqdm import tqdm
 
 
-paths = sorted(glob(join('../data/basic5000', "label_phone_align", "*.lab")))
+paths = sorted(glob(join("../data/basic5000", "label_phone_align", "*.lab")))
 
 for i, filepath in tqdm(enumerate(paths)):
     label = hts.load(filepath)
-
 
     end_times_ = label.end_times
     end_times = []
 
     for j, end_time in enumerate(end_times_):
         str_tmp = str(label[j])
-        if '-a+' in str_tmp or '-i+' in str_tmp or  '-u+' in str_tmp or  '-e+' in str_tmp or  '-o+' in str_tmp or  '-cl+' in str_tmp or  '-N+' in str_tmp or  '-A+' in str_tmp or '-I+' in str_tmp or  '-U+' in str_tmp or  '-E+' in str_tmp or  '-O+' in str_tmp:
-            end_times.append(end_time-1)
+        mora_str = str_tmp[index_mora + 1 : str_label.index("+")]
+        if mora_str not in [
+            "a",
+            "i",
+            "u",
+            "e",
+            "o",
+            "A",
+            "I",
+            "U",
+            "E",
+            "O",
+            "N",
+            "cl",
+        ]:  #'-a+' in str_tmp or '-i+' in str_tmp or  '-u+' in str_tmp or  '-e+' in str_tmp or  '-o+' in str_tmp or  '-cl+' in str_tmp or  '-N+' in str_tmp or  '-A+' in str_tmp or '-I+' in str_tmp or  '-U+' in str_tmp or  '-E+' in str_tmp or  '-O+' in str_tmp:
+            end_times.append(end_time - 1)
 
+    end_index = np.array(end_times) / 50000 - 1
 
-    end_index = np.array(end_times) / 50000
+    mora_index = np.array([0] * int(end_index[-1]))
 
-
-    mora_index = np.array([0]*int(end_index[-1]))
-
-    mora_index = [1 if i in list(end_index.astype(int)) else 0 for i in range(end_index[-1].astype(int)+1)]
+    mora_index = [
+        1 if i in list(end_index.astype(int)) else 0
+        for i in range(end_index[-1].astype(int) + 1)
+    ]
     indices = label.silence_frame_indices().astype(int)
     mora_index = np.delete(mora_index, indices, axis=0)
     mora_index = mora_index.nonzero()
 
-    np.savetxt('../data/basic5000/mora_index/squeezed_mora_index_' + '0'*(4-len(str(i+1))) + str(i+1) + '.csv', mora_index)
+    np.savetxt(
+        "../data/basic5000/mora_index/squeezed_mora_index_"
+        + "0" * (4 - len(str(i + 1)))
+        + str(i + 1)
+        + ".csv",
+        mora_index,
+    )
