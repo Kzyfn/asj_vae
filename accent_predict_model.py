57e058fb5db85c51676111115bfb4d6d42f4f290
Kzyfn <tomakatay@g.ecc.u-tokyo.ac.jp>
2020-07-21 16:56:04 +0900

    a

--- accent_predict_model.py
+++ accent_predict_model.py
@@ -98,13 +98,13 @@ def train_accent_rnn(args, trial=None, test_ratio=1):
                 for j in range(2):
                     tmp.append(torch.from_numpy(data[j]).float().to(device))
                 recon_batch, z, z_unquantized = vqvae_model(tmp[0], tmp[1], data[2], 0)
-                z_train.append(z)
+                z_train.append(z.cpu().numpy())
 
             for data in tqdm(test_loader):
                 for j in range(2):
                     tmp.append(torch.from_numpy(data[j]).float().to(device))
                 recon_batch, z, z_unquantized = vqvae_model(tmp[0], tmp[1], data[2], 0)
-                z_test.append(z)
+                z_test.append(z.cpu().numpy())
         np.savetxt("z_train.csv", z_train)
         np.savetxt("z_test.csv", z_test)
 
